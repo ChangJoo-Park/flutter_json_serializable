9b9c537ecf3a3962e4da8136a12511a977ac7a66
ChangJoo Park <pcjpcj2@gmail.com>
2019-07-12 16:34:20 +0900

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter JSON List',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter JSON List'),
     );
   }
 }
