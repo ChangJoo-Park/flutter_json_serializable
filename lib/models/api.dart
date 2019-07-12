be92ef521363ac382065323dd38c89e3413a3e13
ChangJoo Park <pcjpcj2@gmail.com>
2019-07-12 16:31:15 +0900

    map에서 Post를 사용하도록 변경

--- lib/models/api.dart
+++ lib/models/api.dart
@@ -6,11 +6,13 @@ import 'post.dart';
 Future<List> fetchList() async {
   http.Response response =
       await http.get('https://jsonplaceholder.typicode.com/posts');
-  List<Post> posts =
-      jsonDecode(response.body).map((item) => Post.fromJson(item)).toList();
+  List<Post> posts = jsonDecode(response.body)
+      .map<Post>((item) => Post.fromJson(item))
+      .toList();
   return posts;
 }
 
+/// 단독으로 테스트하려면 이 주석을 제거하세요
 // main() async {
 //   fetchList().then((list) {
 //     print(list);
